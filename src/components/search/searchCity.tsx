f71b8d81b8bdce08b6f8d2c8e0d278dcf13a3036
Sky blue <loveblue1195@gmail.com>
2021-10-20 07:59:31 +0700

    handler key change

--- src/components/search/searchCity.tsx
+++ src/components/search/searchCity.tsx
@@ -8,21 +8,28 @@ type SearchCityProps = {
 }
 export default function SearchCity({onLocationChange}: SearchCityProps) {
     const [search, setSearch] = useState('');
+    const [keyCode, setKeyCode] = useState(0);
 
+    // Handler search change
     function handlerSearchChange(e: any) {
         const {value} = e.target
         setSearch(value)
-        onLocationChange(value)
+        if (keyCode !== 8) { // delete key code
+            onLocationChange(value)
+        }
     }
 
     function submitForm(e: any) {
         e.preventDefault()
-        onLocationChange(search)
+        if (keyCode !== 8) { // delete key code
+            onLocationChange(search)
+        }
     }
+
     return (
         <>
             <form onSubmit={submitForm}>
-                <input type="text" value={search} className="textbox" placeholder="Search" onChange={handlerSearchChange} />
+                <input type="text" value={search} className="textbox" placeholder="Search" onKeyDown={(e) => setKeyCode(e.keyCode)}  onChange={handlerSearchChange} />
                 <button type="submit"><FontAwesomeIcon icon={faSearch} /> </button>
             </form>
         </>
